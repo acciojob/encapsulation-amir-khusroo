b4d2ad4467e87110ac258fbe00b5bcfeeb5a0ed8
amir-khusroo <amirkhusroo250@gmail.com>
2025-01-19 14:18:35 +0530

    solved assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rw = new RWOnly();
+//    System.out.println(rw.name); //'name' has private access in 'com.driver.RWOnly'
+    rw.setName("Amir Khusroo");
+    System.out.println(rw.getName());
+
+  }
 }
